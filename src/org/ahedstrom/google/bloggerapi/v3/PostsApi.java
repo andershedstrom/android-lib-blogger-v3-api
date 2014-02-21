5267bea7486974981b6f26d1fa28f0cdf325f53f
Anders Hedström <anders.hedstroem@gmail.com>
2014-02-21 09:47:28 +0100

    list method takes a optional 'nextPageToken' parameter

--- src/org/ahedstrom/google/bloggerapi/v3/PostsApi.java
+++ src/org/ahedstrom/google/bloggerapi/v3/PostsApi.java
@@ -4,8 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.ahedstrom.google.ApiCallback;
-import org.ahedstrom.google.Response;
+import org.ahedstrom.google.ListResponse;
 import org.ahedstrom.google.auth.OAuth;
+import org.ahedstrom.http.QueryParam;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -37,18 +38,18 @@ public class PostsApi extends BloggerApi {
 		}.start();
 	}
 	
-	public void list(final String blogId, final ApiCallback<Response<List<Posts>>> callback) {
+	public void list(final String blogId, final String nextPageToken, final ApiCallback<ListResponse<List<Posts>>> callback) {
 		new Thread() {
 			@Override
 			public void run() {
 				try {
-					JSONObject json = new JSONObject(invokeGet(String.format(listPathTemplate, blogId)));
-					ArrayList<Posts> lst = new ArrayList<Posts>();
-					Response<List<Posts>> r = new Response<List<Posts>>(lst);
-					String nextPageToken = json.optString("nextPageToken");
-					if (nextPageToken != null && nextPageToken.trim().length() > 0) {
-						r.addExtra("nextPageToken", nextPageToken);
+					QueryParam qp = null;
+					if (nextPageToken != null) {
+						qp = new QueryParam("pageToken", nextPageToken);
 					}
+					JSONObject json = new JSONObject(invokeGet(String.format(listPathTemplate, blogId), qp));
+					ArrayList<Posts> lst = new ArrayList<Posts>();
+					ListResponse<List<Posts>> r = new ListResponse<List<Posts>>(lst, json.optString("nextPageToken"));
 					JSONArray items = json.getJSONArray("items");
 					for (int i=0; i<items.length(); ++i) {
 						lst.add(new Posts(items.getJSONObject(i)));
